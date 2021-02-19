186a6f1b9e041def52b8a01f5bf3ea76e31eaf32
Josh Motta <joshua.a.motta@gmail.com>
2021-02-18 20:35:47 -0500

    Tech logos appear on click

--- src/app/core/about/about.component.ts
+++ src/app/core/about/about.component.ts
@@ -12,6 +12,16 @@ export class AboutComponent implements OnInit {
   title = "PRS Overview";
   user: User = new User();
   userId: number = 0;
+  counter: number = 1;
+  sqlHidden = true;
+  javaHidden = true;
+  springHidden = true;
+  hibernateHidden = true;
+  jsonHidden = true;
+  htmlHidden = true;
+  cssHidden = true;
+  tsHidden = true;
+  angularHidden = true;
 
   constructor(private userSvc: UserService,
               private sysSvc: SystemService) { }
@@ -29,6 +39,31 @@ export class AboutComponent implements OnInit {
             console.log(err);
           }
         );
+  }
+
+  // changes image flag to true and increments counter
+  displayImage() {
+    switch(this.counter) {
+      case 1: this.sqlHidden = false;
+        break;
+      case 2: this.javaHidden = false;
+        break;
+      case 3: this.springHidden = false;
+        break;  
+      case 4: this.hibernateHidden = false;
+        break;
+      case 5: this.jsonHidden = false;
+        break;
+      case 6: this.htmlHidden = false;
+        break;
+      case 7: this.cssHidden = false;
+        break;
+      case 8: this.tsHidden = false;
+        break;
+      case 9: this.angularHidden = false;
+        break;
+    }
+    this.counter++;
   }
 
 }
